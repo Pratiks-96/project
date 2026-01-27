261de42e091afee4ba37cd8118c69e75314b5228
Pratiks-96 <pratiksavant96@gmail.com>
2026-01-27 23:09:43 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Pratik 🚀 Your CI/CD pipeline is working!");
+  res.send("Hello from Pratik Sawant 🚀 TThis project is built and deployed using GitHub Actions and Node.js API  !");
 });
 
 app.get("/health", (req, res) => {
